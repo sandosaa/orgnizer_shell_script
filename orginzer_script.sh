2d855e4db70cb7b938499014745c7603d653bebc
Sondos Mohamed <129817002+sandosaa@users.noreply.github.com>
2025-07-04 19:51:07 +0300

    Update orginzer_script.sh

--- orginzer_script.sh
+++ orginzer_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Hello, It's Sondos!"
+echo "Hello, It's $USER"
 echo "A shell script helps you to orgnize your files" 
 cd ~
-mkdir -p downloaded_items 
-cd /home/sondos/Downloads
+mkdir -p downloaded_items
+cd $XDG_DOWNLOAD_DIR
 mkdir -p photos
 mv -t photos *.jpg *.png *jpeg *webp 2> /dev/null
 mkdir -p PDFs
@@ -13,9 +13,8 @@ mkdir -p Text
 mv *txt Text 2> /dev/null
 mkdir -p study_file
 mv -t study_file math1 python 'Scintific Thinking' 'structured programming''computer fundamentals' concepts 'Numerical Analysis' 2> /dev/null
-mv -t /home/sondos/downloaded_items photos PDFs Text study_file 2> /dev/null
+mv -t "$HOME/downloaded_items" photos PDFs Text study_file 2> /dev/null
 cd ~
-cd downloaded_items 
+cd downloaded_items
 ls
 echo "These are the folders that you have!"
-
